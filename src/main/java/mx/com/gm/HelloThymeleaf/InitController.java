a9f358bff7c5c10de3cec9f56c7b074e974faba1
Patricia Rodríguez <prlongoria@gmail.com>
2022-11-06 11:57:50 +0100

    Add messages from controller and application.properties to index

--- src/main/java/mx/com/gm/HelloThymeleaf/InitController.java
+++ src/main/java/mx/com/gm/HelloThymeleaf/InitController.java
@@ -1,5 +1,6 @@
 package mx.com.gm.HelloThymeleaf;
 //Es un controlador tipo Rest que es el más sencillo que se puede usar
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j //para el manejo de logging
 
 public class InitController {
-    //Método:
+    //Para acceder al mensaje que puse en application.properties:
+
+    @Value("${index.greeting}") //Ojo esta notación es la de springframework, no la de lombok
+    private String greeting;//Así Spring aplica el concepto de inyección de dependencias
+
+
+
+    //Método (ya no tiene que ver con application properties):
 
     @GetMapping("/") //debo poner esta notación para indicar la ruta al siguiente método, en este
-    // caso / que es http://localhost:8080
+    // caso / que es http://localhost:8080. COn esta notación puedo recibir distintos argumentos en los métodos. Yo
+    // no tengo que instanciar una clase, de eso se encarga Spring para que yo sólo tenga que utilizar esas clases
 
-    public String  init(Model model){//vamos a recibir un objeto tipo Model
+    public String  init(Model model){//vamos a recibir un objeto tipo Model. Con la clase Model voy a agregar la info
+        // que quiero compartir con la vista
+
+        //Inicio una variable local:
+        var message = "Hello World with Thymeleaf";//es la variable que voy a compartir utilizando la clase Model
+
         log.info("ejecutanto el controlador Spring MVC");  //Esto puedo hacerlo gracias a la notación de manejo de logging.
 
+        model.addAttribute("message", message); //utilizo la clase de modelo(que es un mapa) para compartir la variable
+        // message, y el método al ser un mapa, lleva el String y el objeto: "llave" y valor
+
+        //También puedo agregar un mensaje en application properties
+
+        model.addAttribute("greeting", greeting);//ahora debo ir a la vista, index, y desplegar esta información
+
         return "Index"; //Voy a crear un archivo llamado html para que esto retorne una página. Lo creo en
-        // src-main-resources-templates-new-htmlFile
+        // src-main-resources-templates-new-htmlFile. ESTE ARCHIVO ES MI VISTA, DESDE DONDE VOY A LLAMAR A LAS
+        // VARIABLES DE ESTE CONTROLADOR, EL MODELO LO PONE SPRING EN AUTOMÁTICO
+
     }
 }
